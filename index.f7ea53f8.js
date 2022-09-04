d0c05707d71cd7b07e7ab066ed51fd40e8dea44b
Dranyk <96312895+Dranyk@users.noreply.github.com>
2022-09-04 17:10:03 +0000

    Deploying to gh-pages from @ Dranyk/ice-cream@7c74a95c2eb4e7dc23ba94b82b01fd4e6bf9b7a4 🚀

--- index.f7ea53f8.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openMenuBtn:document.querySelector("[data-menu-open-header]"),closeMenuBtn:document.querySelector("[data-menu-close-header]"),menu:document.querySelector("[data-menu-header]")};function n(){e.menu.classList.toggle("is-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
-//# sourceMappingURL=index.f7ea53f8.js.map
