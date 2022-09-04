663c63edf26dcf465db789b6be6f702b0c5fb16d
Dranyk <96312895+Dranyk@users.noreply.github.com>
2022-09-04 13:48:50 +0000

    Deploying to gh-pages from @ Dranyk/ice-cream@0ac9d0cb6cd5f0a6404125982ec24cb7d40333e7 🚀

--- index.12dc6f6f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuBtn:document.querySelector("[data-menu-open-header]"),closeMenuBtn:document.querySelector("[data-menu-close-header]"),menu:document.querySelector("[data-menu-header]")};function n(){e.menu.classList.toggle("visually-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.12dc6f6f.js.map
--- index.a92d9662.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openMenuBtn:document.querySelector("[data-menu-open-header]"),closeMenuBtn:document.querySelector("[data-menu-close-header]"),menu:document.querySelector("[data-menu-header]")};function n(){e.menu.classList.toggle("visually-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)})();
-//# sourceMappingURL=index.a92d9662.js.map
