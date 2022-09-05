0a17e1599eddf8a79789c1b2f8d05df55945cbf9
Dranyk <dvdranyk@gmail.com>
2022-09-05 19:38:16 +0300

    js

--- src/js/Header_modal.js
+++ src/js/Header_modal.js
@@ -1,14 +1,14 @@
 (() => {
-                    const refs = {
-                        openModalBtn: document.querySelector("[data-modal-open-header]"),
-                        closeModalBtn: document.querySelector("[data-modal-close-header]"),
-                        modal: document.querySelector("[data-modal-header]"),
-                    };
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open-header]"),
+        closeModalBtn: document.querySelector("[data-modal-close-header]"),
+        modal: document.querySelector("[data-modal-header]"),
+    };
 
-                    refs.openModalBtn.addEventListener("click", toggleModal);
-                    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
 
-                    function toggleModal() {
-                        refs.modal.classList.toggle("is-hidden");
-                    }
-                })();
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
+})();
